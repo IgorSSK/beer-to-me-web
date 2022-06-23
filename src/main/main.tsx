d1393c673cac186606b1385f8632641c172a7917
Igor Souza <igorsouza.96@hotmail.com>
2022-06-23 01:44:58 -0300

    change root element for React 18+

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,11 +1,12 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import PublishmentList from '../presentation/views/publishment';
 
-ReactDOM.render(
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
 	<React.StrictMode>
 		<PublishmentList />
 	</React.StrictMode>,
-	document.getElementById('root')
 );
